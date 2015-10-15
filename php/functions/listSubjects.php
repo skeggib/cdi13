7f01f1b3ecd70bc811a6ffbb5dab8a993fcb7823
Sebastien Klasa <skeggib@gmail.com>
2015-10-15 09:51:45 +0200

    [Back] correction de la fonction getSubjects

--- php/functions/listSubjects.php
+++ php/functions/listSubjects.php
@@ -1,12 +1,6 @@
 <?php
 
-function getSubjectsArray(/*$subjectId*/) {
-	//TODO SKEGGIB : Tu ne peux pas faire ca comme ca (J'ai commenté pour eviter une erreur).
-	//Crée une autre méthode "getSubjectById"
-	//Et je pense que celle ci devrait juste s'appeler "GetSubjects"
-
-	//Sinon tu fais comme dans listLinks / GetLinks
-
+function getSubjectsArray() {
 	$query = "SELECT id, name FROM subjects";
 	$results = pg_query($query);
 
